2db987fa8bc51861c3c5213bf1c14bdab8f2e2ad
Renjini.Radhakrishnan1 <Renjini.Radhakrishnan1@ust.com>
2021-12-18 03:40:46 +0530

    Update

--- RestaurantProject/src/app/app.module.ts
+++ RestaurantProject/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
+import { MenuComponent } from './menu/menu.component';
+import { AboutComponent } from './about/about.component';
+import { EventComponent } from './event/event.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavComponent,
+    MenuComponent,
+    AboutComponent,
+    EventComponent
   ],
   imports: [
     BrowserModule,
